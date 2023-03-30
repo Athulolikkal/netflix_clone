c485bb466a1d2d1fdc6fc9d90ef948de000ae228
Athul Sabu <116870115+Athulolikkal@users.noreply.github.com>
2023-03-30 10:38:39 +0530

    add map method in app.js for row post component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import React from 'react'
 import './App.css';
 import Banner from './Components/Navbar/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar'
-import {actions,originals,comedy,horrer,romance,trending,documentaries} from './urls'
+import {genres} from './urls'
 import './Components/Navbar/Navbar.css'
 import RowPost from './Components/Navbar/RowPost/RowPost';
 
 
 function App() {
-  return (
+
+   return (
     <div className="App">
       <Navbar />
       <Banner />
-      <RowPost url={originals} title="Netflix Originals" />
-      <RowPost url={trending} title='Trending' isSmall />
-      <RowPost url={comedy} title='Comedy' isSmall />
-      <RowPost url={romance} title='Romance' isSmall />
-      <RowPost url={actions} title='Actions' isSmall />
-      
-      <RowPost url={horrer} title='Horrer' isSmall />
+
+     {genres.map((item)=>{
+      return(
+        <RowPost url={item.url} title={item.title} isSmall={item.isSmall} />
+      )
+     })}
      
-      <RowPost url={documentaries} title='Documentaries' isSmall />
     </div>
   );
 }
--- src/urls.js
+++ src/urls.js
@@ -1,9 +1,34 @@
 import { API_KEY } from './Constants/Constants';
 
-export const originals=`discover/tv?api_key=${API_KEY}&with_networks=213`;
-export const actions=`discover/movie?api_key=${API_KEY}&with_genres=28`;
-export const comedy=`discover/movie?api_key=${API_KEY}&with_genres=35`;
-export const horrer=`discover/movie?api_key=${API_KEY}&with_genres=27`;
-export const romance=`discover/movie?api_key=${API_KEY}&with_genres=10749`;
-export const trending=`trending/all/week?api_key=${API_KEY}&language=en-US`;
-export const documentaries=`discover/movie?api_key=${API_KEY}&with_genres=99`;
+export const genres = [
+    {url:`discover/tv?api_key=${API_KEY}&with_networks=213`,
+     title:'Netflix Originals ',  
+    isSmall:false},
+    
+    {url:`trending/all/week?api_key=${API_KEY}&language=en-US`,
+    title:'Trending ',
+    isSmall:true},
+
+    {url:`discover/movie?api_key=${API_KEY}&with_genres=35`,
+    title:'Comedy',
+    isSmall:true},
+   
+    {url:`discover/movie?api_key=${API_KEY}&with_genres=28`,
+    title:'Actions',
+    isSmall:true},
+   
+    {url:`discover/movie?api_key=${API_KEY}&with_genres=27`,
+    title:'Horrer',
+    isSmall:true},
+   
+    {url:`discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    title:'Romance',
+    isSmall:true},
+   
+    {url:`discover/movie?api_key=${API_KEY}&with_genres=99`,
+    title:'Documentaries',
+    isSmall:true},
+
+
+]
+
